ab52369098d32c7d7567152bbdc542305fe74a8d
Mahmud4M <mahmud57555@gmail.com>
2024-05-03 21:46:38 +0600

    Completed updated{PATCH] for checout section

--- index.js
+++ index.js
@@ -52,10 +52,12 @@ async function run() {
             const options = {
                 // Include only the `title` and `imdb` fields in the returned document
                 projection: {
+                    customerName: 1,
                     service_id: 1,
                     title: 1,
                     price: 1,
-                    
+                    img: 1
+
                 },
             };
             const result = await servicesCollection.findOne(query, options);
@@ -63,10 +65,45 @@ async function run() {
         })
 
         // Get booking data
+        app.get('/bookings', async (req, res) => {
+            let = query = {};
+            if (req.query?.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+
+            const result = await bookingCollection.find(query).toArray();
+            res.send(result)
+        })
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             const result = await bookingCollection.insertOne(booking);
             res.send(result)
+        })
+
+        // Delete 
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // Update cofirm button
+        app.patch('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)}
+            const updatedBooking = req.body;
+            console.log(updatedBooking)
+            const updateDoc = {
+                $set: {
+                    status: updatedBooking.status
+                },
+              };
+              const result = await bookingCollection.updateOne(filter, updateDoc)
+              res.send(result)
         })
 
 
